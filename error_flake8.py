467db97a79e2b296e2ac9bab52da9bd9c5aba116
AlsoloProg <alexandersolislopez@gmail.com>
2021-03-04 15:53:18 -0600

    Codigo con errores flake8
    Codigo con cuatro errores de flake8 que verifica si la suma de dos numeros es o no 40.

---
+++ error_flake8.py
@@ -0,0 +1,22 @@
+import os
+
+
+def is40(numA, numB):  # Verifica si la suma de dos numeros es 40
+    unnecesaryVariable = numA + 5
+    addition = numA + numB  # Suma los numeros ingresados
+    if(addition==40):
+        return True
+    else:
+        return False
+
+
+
+
+
+firstNum = 20
+secondNum = 20
+if(is40(firstNum, secondNum)):  # Suma 20 y 20, obteniendo 40 e
+    # imprimiendo el mensaje correspondiente
+    print("¡Es 40!")
+else:
+    print("No es 40")
